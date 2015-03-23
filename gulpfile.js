df09e4c93c75dc5bedab0476bdce071df031f66e
Andrii Korzh <Andrii.Korzh@gmail.com>
2015-03-23 18:32:02 +0200

    Update Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-var gulp       = require('gulp')
-  , gutil      = require('gulp-util')
-  , coffee     = require('gulp-coffee')
-  , concat     = require('gulp-concat')
-  , uglify     = require('gulp-uglify')
-  , jade       = require('gulp-jade')
-  , stylus     = require('gulp-stylus')
-  , imagemin   = require('gulp-imagemin')
-  , exec       = require('gulp-exec')
-  , livereload = require('gulp-livereload')
-  , plumber    = require('gulp-plumber');
-
-var nib = require('nib');
+var fs         = require('fs');
+var path       = require('path');
+var nib        = require('nib');
+var gulp       = require('gulp');
+var gutil      = require('gulp-util');
+var coffee     = require('gulp-coffee');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var jade       = require('gulp-jade');
+var stylus     = require('gulp-stylus');
+var imagemin   = require('gulp-imagemin');
+var exec       = require('gulp-exec');
+var livereload = require('gulp-livereload');
+var plumber    = require('gulp-plumber');
 
 var port = process.env.PORT || 5000;
 
@@ -25,48 +26,50 @@ var paths = {
 
 gulp.task('vendor', function() {
   return gulp.src(paths.vendor)
-    .pipe(gulp.dest(paths.dest + '/vendor'));
+    .pipe(gulp.dest(paths.dest + '/vendor'))
+    .pipe(livereload());
 });
 
 gulp.task('js', function() {
   return gulp.src(paths.js)
     .pipe(coffee({ bare: true }).on('error', gutil.log))
-    .pipe(gulp.dest(paths.dest + '/js'));
+    .pipe(gulp.dest(paths.dest + '/js'))
+    .pipe(livereload());
 });
 
 gulp.task('images', function() {
  return gulp.src(paths.images)
     .pipe(imagemin({ optimizationLevel: 5 }))
-    .pipe(gulp.dest(paths.dest + '/img'));
+    .pipe(gulp.dest(paths.dest + '/img'))
+    .pipe(livereload());
 });
 
 gulp.task('css', function () {
   gulp.src(paths.css)
     .pipe(plumber())
     .pipe(stylus({ use: ['nib'], import: ['nib'] })).on('error', gutil.log)
-    .pipe(gulp.dest(paths.dest + '/css'));
+    .pipe(gulp.dest(paths.dest + '/css'))
+    .pipe(livereload());
 });
 
 gulp.task('jade', function() {
   return gulp.src(paths.jade)
     .pipe(jade({ pretty: true }).on('error', gutil.log))
-    .pipe(gulp.dest(paths.dest));
+    .pipe(gulp.dest(paths.dest))
+    .pipe(livereload());
 });
 
 gulp.task('connect', function(next) {
-  var connect = require('connect')
-    , fs = require('fs')
-    , path = require('path')
-    , server = connect();
-  server.use(connect.static(paths.dest)).listen(port, function(){
-    gutil.log('Connect listen on', port);
-    next();
-  });
-  server.use('/', function(req, res, next){
-    var file = paths.dest + '/' + (req._parsedUrl.pathname.slice(1) || 'index') + '.html';
-    fs.createReadStream(file).on('error', next).pipe(res);
-  });
-  server.use(function(err, req, res, next){ res.statusCode = 404; res.end(); })
+  var connect = require('connect');
+  var server = connect()
+  var static = require('serve-static');
+
+  server
+    .use(static(paths.dest))
+    .listen(port, function(){
+      gutil.log('Connect listen on', port);
+      next();
+    });
 });
 
 // Rerun the task when a file changes
@@ -76,11 +79,7 @@ gulp.task('watch', ['connect'], function() {
   gulp.watch(paths.css, ['css']);
   gulp.watch(paths.jade, ['jade']);
   gulp.watch(paths.vendor, ['vendor']);
-
-  var server = livereload();
-  gulp.watch(paths.dest + '/**').on('change', function(file) {
-      server.changed(file.path);
-  });
+  livereload.listen();
 });
 
 gulp.task('default', ['images', 'vendor', 'js', 'css', 'jade', 'watch']);
